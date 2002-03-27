76746f57d609dbeae2aa78012143f0bf73befa33
Adam Fedor <fedor@gnu.org>
2002-03-27 23:44:33 +0000

    Initial backend version (extracted from xgps)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/back/branches/GNUSTEP@13248 72102866-910b-0410-8b05-ffd578937521

---
+++ config.h
@@ -0,0 +1,28 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if the X Window System is missing or not being used.  */
+/* #undef X_DISPLAY_MISSING */
+
+#define XSHM 1
+
+#define WITH_WRASTER 1
+
+#define HAVE_XFT 1
+
+/* #undef USE_XIM */
+
+/* #undef BUILD_XLIB */
+#define BUILD_XDPS 1
+#define BUILD_X11 1
+/* #undef BUILD_WIN32 */
+/* #undef BUILD_WINLIB */
+
+/* Define if you have the <DPS/dpsNXargs.h> header file.  */
+#define HAVE_DPS_DPSNXARGS_H 1
+
+/* Define if you have the <DPS/dpsclient.h> header file.  */
+#define HAVE_DPS_DPSCLIENT_H 1
+
+/* Define if you have the <wraster.h> header file.  */
+/* #undef HAVE_WRASTER_H */
