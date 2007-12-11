4e6f30b8c081cdab8ad81094c94276fe454c9143
ipaqah <ipaqah@72102866-910b-0410-8b05-ffd578937521>
2007-12-11 12:06:42 +0000

    Reverting back to former font system.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/back/trunk@25721 72102866-910b-0410-8b05-ffd578937521

--- Headers/cairo/CairoFaceInfo.h
+++ Headers/cairo/CairoFaceInfo.h
@@ -39,47 +39,28 @@
 @interface CairoFaceInfo : NSObject
 {
 	int _weight;
-	float _italicAngle;
 	unsigned int _traits;
-	
-	FT_Library _ftlibrary;
-	FT_Face _ftface;
 
-	cairo_font_face_t *_fontFace;
-	
-	NSArray *_filePaths;
-	int _indexInFile;
+	cairo_font_face_t *_fontFace; 
+  FcPattern *_pattern;
 
 	NSString *_familyName;
-	NSString *_fullName;
 }
 
-- (id) initWithfamilyName: (NSString *)familyName
-                 fullName: (NSString *)fullName
+- (id) initWithfamilyName: (NSString *)familyName 
                    weight: (int)weight 
-              italicAngle: (float)italicAngle
                    traits: (unsigned int)traits 
-										files: (NSArray *)paths
-                    index: (int)index;
+                  pattern: (FcPattern *)pattern;
 
 - (unsigned int) cacheSize;
 
 - (int) weight;
 - (void) setWeight: (int)weight;
-- (int) italicAngle;
-- (void) setItalicAngle: (float)italicAngle;
 - (unsigned int) traits;
 - (void) setTraits: (unsigned int)traits;
 
 - (NSString *) familyName;
 - (void) setFamilyName: (NSString *)name;
-- (NSString *)fullName;
-- (void) setFullName: (NSString *)name;
-
-- (void) setFiles: (NSArray *)path;
-- (NSArray *)files;
-- (void) setIndex: (int)index;
-- (int)index;
 
 - (cairo_font_face_t *)fontFace;
 
