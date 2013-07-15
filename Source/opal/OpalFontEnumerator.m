fbe9ad10329ea6b61a70c9f1d239be549df053f9
Ivan Vučica <ivan@vucica.net>
2013-07-15 15:24:04 +0000

    opal: Implementation of a lot of drawing and matrix methods. Work on getting backing store support to work.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/back/trunk@36893 72102866-910b-0410-8b05-ffd578937521

--- Source/opal/OpalFontEnumerator.m
+++ Source/opal/OpalFontEnumerator.m
@@ -27,11 +27,88 @@
 
 #import "opal/OpalFontEnumerator.h"
 
+#import <Foundation/Foundation.h>
+
+@interface OpalFaceInfo : NSObject
+/* DUMMY interface */
+{
+  NSString * _familyName;
+  int _weight;
+  unsigned int _traits;
+}
+- (id) initWithFamilyName: (NSString *)familyName 
+                   weight: (int)weight 
+                   traits: (unsigned int)traits;
+
+- (unsigned int) cacheSize;
+
+- (int) weight;
+- (void) setWeight: (int)weight;
+- (unsigned int) traits;
+- (void) setTraits: (unsigned int)traits;
+
+- (NSString *) familyName;
+- (void) setFamilyName: (NSString *)name;
+
+- (NSCharacterSet*)characterSet;
+@end
+@implementation OpalFaceInfo
+/* DUMMY implementation */
+- (id) initWithFamilyName: (NSString *)familyName 
+                   weight: (int)weight 
+                   traits: (unsigned int)traits 
+{
+  self = [super init];
+  if (!self)
+    return nil;
+  
+  _familyName = [familyName retain];
+  _weight = weight;
+  _traits = traits;
+  
+  return self;
+}
+- (int) weight { return _weight; }
+- (void) setWeight: (int)weight { _weight = weight; }
+- (unsigned int) traits { return _traits; }
+- (void) setTraits: (unsigned int)traits { _traits = traits; }
+- (NSString *)familyName { return _familyName; }
+- (void) setFamilyName: (NSString *)name { [_familyName release]; _familyName = [name retain]; }
+
+- (NSCharacterSet *) characterSet { return nil; }
+@end
+
+
+
 @implementation OpalFontEnumerator
+
++ (OpalFaceInfo *) fontWithName: (NSString *) name
+{
+  return [[[OpalFaceInfo alloc] initWithFamilyName:name weight:1 traits:0] autorelease];
+}
 
 - (void) enumerateFontsAndFamilies
 {
+  allFontNames = [[NSArray arrayWithObjects: @"FreeSans", 
+                   @"FreeSans-Bold", @"FreeMono", nil] retain];
+  allFontFamilies = [[NSDictionary dictionaryWithObjectsAndKeys:
+                      @"FreeSans", @"FreeSans",
+                      @"FreeMono", @"FreeMono",
+                      nil] retain];
+  
 }
 
+- (NSString *) defaultSystemFontName
+{
+  return @"FreeSans";
+}
+- (NSString *) defaultBoldSystemFontName
+{
+  return @"FreeSans-Bold";
+}
+- (NSString *) defaultFixedPitchFontName
+{
+  return @"FreeMono";
+}
 @end
 
--- Source/opal/OpalSurface.m
+++ Source/opal/OpalSurface.m
@@ -32,6 +32,66 @@
 extern CGContextRef OPX11ContextCreate(Display *display, Drawable drawable);
 extern void OPContextSetSize(CGContextRef ctx, CGSize s);
 
+/* Taken from GSQuartzCore's CABackingStore */
+static CGContextRef createCGBitmapContext (int pixelsWide,
+                                             int pixelsHigh)
+{
+  CGContextRef    context = NULL;
+  CGColorSpaceRef colorSpace;
+  void *          bitmapData;
+  int             bitmapByteCount;
+  int             bitmapBytesPerRow;
+  
+  bitmapBytesPerRow   = (pixelsWide * 4);
+  bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
+  
+  colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);// 2
+
+  // Let CGBitmapContextCreate() allocate the memory.
+  // This should be good under Cocoa too.
+  bitmapData = NULL;
+
+  context = CGBitmapContextCreate (bitmapData,
+                                   pixelsWide,
+                                   pixelsHigh,
+                                   8,      // bits per component
+                                   bitmapBytesPerRow,
+                                   colorSpace,
+#if !GNUSTEP
+                                   kCGImageAlphaPremultipliedLast);
+#else
+  // Opal only supports kCGImageAlphaPremultipliedFirst.
+  // However, this is incorrect since it implies ARGB.
+                                  kCGImageAlphaPremultipliedFirst);
+#endif
+
+  // Note: our use of premultiplied alpha means that we need to
+  // do alpha blending using:
+  //  GL_SRC_ALPHA, GL_ONE
+
+  CGColorSpaceRelease(colorSpace);
+  if (context== NULL)
+    {
+      free (bitmapData);// 5
+      fprintf (stderr, "Context not created!");
+      return NULL;
+    }
+
+#if GNUSTEP
+#warning Opal bug: context should be cleared automatically
+
+#if 0
+  CGContextClearRect (context, CGRectInfinite);
+#else
+#warning Opal bug: CGContextClearRect() permanently whacks the context
+  memset (CGBitmapContextGetData (context), 
+          0, bitmapBytesPerRow * pixelsHigh);
+#endif
+#endif  
+  return context;
+}
+
+
 @implementation OpalSurface
 
 - (id) initWithDevice: (void *)device
@@ -46,8 +106,31 @@ extern void OPContextSetSize(CGContextRef ctx, CGSize s);
 
   Display * display = _gsWindowDevice->display;
   Window window = _gsWindowDevice->ident;
+  _x11CGContext = OPX11ContextCreate(display, window);
+  
+  if (_gsWindowDevice->type == NSBackingStoreNonretained)
+    {
+      // Don't double-buffer:
+      // use the window surface as the drawing destination.
+    }
+  else
+    {
+      // Do double-buffer:
+      // Create a similar surface to the window which supports alpha
 
-  _cgContext = OPX11ContextCreate(display, window);
+      // Ask XGServerWindow to call +[OpalContext handleExposeRect:forDriver:]
+      // to let us handle the back buffer -> front buffer copy using Opal.
+      _gsWindowDevice->gdriverProtocol |= GDriverHandlesExpose | GDriverHandlesBacking;
+      _gsWindowDevice->gdriver = self;
+
+#if 0
+      // Disabled because when the content is blitted onto the screen,
+      // we get just yellow blots instead of actual content.
+      _backingCGContext = createCGBitmapContext(
+                       _gsWindowDevice->buffer_width, 
+                       _gsWindowDevice->buffer_height);
+#endif
+    }
   
   return self;
 }
@@ -57,18 +140,53 @@ extern void OPContextSetSize(CGContextRef ctx, CGSize s);
   return _gsWindowDevice;
 }
 
+- (CGContextRef) cgContext
+{
+  return _backingCGContext ? _backingCGContext : _x11CGContext;
+}
+
+- (void) handleExposeRect: (NSRect)rect
+{
+  CGRect cgRect = CGRectMake(rect.origin.x, rect.origin.y, 
+                      rect.size.width, rect.size.height);
+  
+  CGImageRef backingImage = CGBitmapContextCreateImage(_backingCGContext);
+  if (!backingImage) // FIXME: writing a nil image fails with Opal
+    return;
+    
+  CGContextDrawImage(_x11CGContext, cgRect, backingImage);
+  
+  // FIXME: Opal tries to access -path from CFURLRef
+  //CFURLRef fileUrl = CFURLCreateWithFileSystemPath(NULL, @"/tmp/opalback.jpg", kCFURLPOSIXPathStyle, NO);
+  CFURLRef fileUrl = (CFURLRef)[[NSURL fileURLWithPath: @"/tmp/opalback.jpg"] retain];
+  CGImageDestinationRef outfile = CGImageDestinationCreateWithURL(fileUrl, @"public.jpeg"/*kUTTypeJPEG*/, 1, NULL);
+  CGImageDestinationAddImage(outfile, backingImage, NULL);
+  CGImageDestinationFinalize(outfile);
+  CFRelease(fileUrl);
+  CFRelease(outfile);
+  
+  
+  CGImageRelease(backingImage);
+}
+
+- (BOOL) isDrawingToScreen
+{
+  // TODO: stub
+  return YES;
+}
+
 - (void) dummyDraw
 {
 
   NSLog(@"performing dummy draw");
   
-  CGContextSaveGState(_cgContext);
+  CGContextSaveGState([self cgContext]);
 
   CGRect r = CGRectMake(0, 0, 1024, 1024);
-  CGContextSetRGBFillColor(_cgContext, 1, 0, 0, 1);
-  CGContextFillRect(_cgContext, r);
+  CGContextSetRGBFillColor([self cgContext], 1, 0, 0, 1);
+  CGContextFillRect([self cgContext], r);
 
-  CGContextRestoreGState(_cgContext);
+  CGContextRestoreGState([self cgContext]);
 
 }
 
