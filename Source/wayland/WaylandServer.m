360cb8ed95256cd754f6f4f0a21d8b0c332af237
pkgdemon <pkgdemonteam@gmail.com>
2024-08-28 15:07:16 -0500

    Use window parameter directly

--- Source/wayland/WaylandServer.m
+++ Source/wayland/WaylandServer.m
@@ -781,7 +781,7 @@ WaylandServer (SurfaceRoles)
       break;
     case NSPopUpMenuWindowLevel:
       NSDebugLog(@"[%d] NSPopUpMenuWindowLevel", win);
-      [self createPopup:get_window_with_id(wlconfig, win)];
+      [self createPopup:window];
       break;
     case NSScreenSaverWindowLevel:
       NSDebugLog(@"[%d] NSScreenSaverWindowLevel", win);
