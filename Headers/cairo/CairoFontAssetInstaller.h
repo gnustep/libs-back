aa103ff06c7531335c012de8269bf1268f614c27
Gregory John Casamento <greg.casamento@gmail.com>
2025-10-05 21:26:45 -0400

    Add concrete implementations for backend

---
+++ Headers/cairo/CairoFontAssetInstaller.h
@@ -0,0 +1,36 @@
+/*
+   CairoFontAssetInstaller.h
+
+   Copyright (C) 2003 Free Software Foundation, Inc.
+
+   By: Gregory John Casamento <greg.casamento@gmail.com>
+   Date: October 4, 2025
+
+   This file is part of GNUstep.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef CairoFontAssetInstaller_h
+#define CairoFontAssetInstaller_h
+
+#include "fontconfig/FCFontAssetInstaller.h"
+
+@interface CairoFontAssetInstaller : FCFontAssetInstaller
+@end
+
+#endif
