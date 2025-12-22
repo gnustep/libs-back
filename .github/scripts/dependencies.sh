7ebb07f0a93d0d830a7454fbf68dcf52d7ba68bd
Riccardo <rm@gnu.org>
2025-12-22 22:08:08 +0100

    use installed GNUstep.sh

--- .github/scripts/dependencies.sh
+++ .github/scripts/dependencies.sh
@@ -4,7 +4,7 @@ set -ex
 install_gnustep_gui() {
     echo "::group::GNUstep Gui"
     cd $DEPS_PATH
-    . $INSTALL_PATH/share/GNUstep/Makefiles/GNUstep.sh
+    . /usr/share/GNUstep/Makefiles/GNUstep.sh
     git clone -q -b ${LIBS_BASE_BRANCH:-master} https://github.com/gnustep/libs-gui.git
     cd libs-gui
     ./configure
